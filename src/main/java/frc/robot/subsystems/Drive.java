06b4bdd4949dc9a29bd5374996cd75d7f4352e77
omniscient-cookie <vainsencher.or@gmail.com>
2025-03-24 15:37:49 -0400

    updated jsons and made basic YAGSL drivetrain

--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -1,7 +1,30 @@
 package frc.robot.subsystems;
+import java.io.File;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import swervelib.SwerveDrive;
+import swervelib.parser.SwerveParser;
+import edu.wpi.first.math.kinematics.Kinematics;
+
+
 
 public class Drive extends SubsystemBase{
-    public Drive(){}
+    double maximumSpeed = Units.feetToMeters(13);
+    File swerveJsonDirectory = new File(Filesystem.getDeployDirectory(),"swerve");
+    SwerveDrive swerveDrive;
+    
+    public Drive(){
+        try {
+            swerveDrive = new SwerveParser(new File("src//main//deploy//swerve")).createSwerveDrive(maximumSpeed);
+          }
+          catch(Exception IOException) {}
+        
+    }
+    public void driveRobotRelative(Translation2d translation,double rotation){
+        swerveDrive.drive(translation,rotation,false,false);
+    }
     
 }
